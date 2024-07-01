5f1f64abfa2d5505a2e36106e7075f9909f2ba22
VicVerevita <0victor.gabriel01@gmail.com>
2024-07-01 20:54:10 +0000

    start ui functionalities implementation

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,3 +1,5 @@
+use crate::config::AppState;
+
 pub enum CurrentScreen {
     Register,
     Login,
@@ -36,6 +38,26 @@ impl App {
             tasks: Vec::new(),
             current_screen: CurrentScreen::Main,
             currently_editing: None,
+        }
+    }
+
+    pub fn login(&mut self) {
+        if AppState::
+    }
+
+    pub fn toggle_editing(&mut self) {
+        if let Some(edit_mode) = &self.currently_editing {
+            match edit_mode {
+                CurrentlyEditing::Title => {
+                    self.currently_editing = Some(CurrentlyEditing::Description)
+                }
+                CurrentlyEditing::Description => {
+                    self.currently_editing = Some(CurrentlyEditing::Status)
+                }
+                CurrentlyEditing::Status => todo!(),
+            };
+        } else {
+            self.currently_editing = Some(CurrentlyEditing::Title);
         }
     }
 }
